ba4044b8c6bff49c69fe5854b3718bd707abf921
Floriancrt <florian.courthieu@gmail.com>
2019-09-20 14:31:54 +0200

    FIX: Final Angular Routing

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
-import { ProductDetailsComponent } from './products/product-details/product-details.component';
 import { ProductsComponent } from './products/products.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 @NgModule({
